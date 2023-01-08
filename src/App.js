5f2ef013e44f4976b5b984dec6c20ded4d72a47a
duncan982 <ndegwaduncan@icloud.com>
2023-01-08 18:27:08 +0100

    minor changes: removed unused codes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React from "react";
 import "./App.css";
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-import Home from "./Home";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import Header from "./Header";
 
-// function App() {
-//   return (
-//     <div className="app">
-//       <Header />
-//       <Router>
-//         <Routes>
-//           <Route>
-//             <Route path="/" element={<Home />} />
-//           </Route>
-//         </Routes>
-//       </Router>
-//     </div>
-//   );
-// }
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
         {" "}
-        // You don't *have* to wrap the Outlet in a main tag, but I am doing, to
-        save having to remember to do it in each component
         <Outlet />
       </main>
     </div>
--- src/index.js
+++ src/index.js
@@ -5,13 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Home from "./Home";
 
-import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -26,21 +20,5 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<App />}>
-//           <Route index element={<Home />} />
-//           {/* <Route path="projects" element={<Projects />} /> */}
-//         </Route>
-//       </Routes>
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
